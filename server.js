5e6038dce8969cf93752cc705822aec985b7373f
Keesun Baik <whiteship@epril.com>
2012-03-01 02:31:07 +0900

    낙서장 개발 끝

--- server.js
+++ server.js
@@ -4,45 +4,51 @@ var httpClient = require('http').createClient(8080, 'localhost');
 var chat = io
     .of('/chat')
     .on('connection', function (socket) {
-        socket.on('message', function(data){
-            console.log(data);
-        });
+      socket.on('message', function (data) {
+        console.log(data);
+      });
 
-        socket.on('chat', function(data){
-            chat.emit('message', data);
-        });
+      socket.on('chat', function (data) {
+        chat.emit('message', data);
+      });
 
-        socket.on('getIn', function(data){
-            // get into Spring
-            var req = httpClient.request('GET', '/chat/in?sock=' + socket.id + '&email=' + data.email);
-            req.end();
-            req.on('response', function(response){
-                response.on('data', function (chunk) {
+      socket.on('getIn', function (data) {
+        // get into Spring
+        var req = httpClient.request('GET', '/chat/in?sock=' + socket.id + '&email=' + data.email);
+        req.end();
+        req.on('response', function (response) {
+          response.on('data', function (chunk) {
 //                    console.log('BODY: ' + chunk);
-                    chat.emit('refresh', {msg:'update'});
-                });
-            });
-            chat.emit('message', data);
+            chat.emit('refresh', {msg:'update'});
+          });
+        });
+        chat.emit('message', data);
 //            console.log(data);
-        });
+      });
 
-        socket.on('disconnect', function () {
-            // get into Spring
-            var req = httpClient.request('GET', '/chat/out?sock=' + socket.id);
-            req.end();
-            req.on('response', function(response){
-                response.on('data', function (chunk) {
+      socket.on('disconnect', function () {
+        // get into Spring
+        var req = httpClient.request('GET', '/chat/out?sock=' + socket.id);
+        req.end();
+        req.on('response', function (response) {
+          response.on('data', function (chunk) {
 //                    console.log('BODY: ' + chunk);
-                    chat.emit('refresh', {msg:'update'});
-                });
-            });
+            chat.emit('refresh', {msg:'update'});
+          });
         });
+      });
 
-        socket.emit('message', { who: 'springsprout', msg: '방가방가' });
+      socket.emit('message', { who:'springsprout', msg:'방가방가' });
     });
 
-var news = io
-    .of('/news')
+var suda = io
+    .of('/suda')
     .on('connection', function (socket) {
-        socket.emit('item', { news:'item' });
+      socket.on('message', function(data){
+        // save to server
+        var req = httpClient.request('GET', '/suda/add?userId=' + data.id + "&message=" + data.msg);
+        req.end();
+
+        suda.emit('message', data);
+      });
     });
